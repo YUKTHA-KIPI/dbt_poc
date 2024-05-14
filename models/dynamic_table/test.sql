9fed7afa7fde9d001484b4b1b84af32732a3b61b
YUKTHA-KIPI <104973913+YUKTHA-KIPI@users.noreply.github.com>
2024-05-14 16:11:54 +0530

    Update test.sql

--- models/dynamic_table/test.sql
+++ models/dynamic_table/test.sql
@@ -1,8 +1,19 @@
+{%if target.name == 'QA' %}
+    {% set warehouse=var('qa_wh')%}
+{%elif target.name == 'PROD' %}
+    {% set warehouse=var('prod_wh') %}
+{%else%}
+    {% set warehouse=var('dev_wh') %}
+{%endif %}
+
 {{config(
-    materialized = 'view',
+    materialized = 'dynamic_table',
+    snowflake_warehouse = warehouse,
+    target_lag = '1 hour',
     on_configuration_change = 'apply'
 )}}
    WITH test AS (
-     select * from DBT_HOL_DEV.INFORMATION_SCHEMA.DATABASES 
+     select * from DBT.INFORMATION_SCHEMA.SCHEMATA 
    )
    SELECT * FROM test
+
