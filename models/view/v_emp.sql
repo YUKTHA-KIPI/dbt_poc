54e3a4f3438a9d3506b6ee76835337bf3974db5a
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2024-05-24 14:26:47 +0530

    state

--- models/view/v_emp.sql
+++ models/view/v_emp.sql
@@ -1,11 +1,7 @@
 with employee_details AS (
     SELECT 
     employee_id,
-<<<<<<< HEAD
     CONCAT(first_name,' ', last_name) AS name,
-=======
-    CONCAT(first_name,'   ', last_name) AS full_name,
->>>>>>> b527ce66c5fdaf8f19789daae6d7ed8ae12657a2
     department_id,
     salary
 FROM 
