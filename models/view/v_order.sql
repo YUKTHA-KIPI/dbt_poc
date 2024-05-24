706d9dbae8a74522bc9ff9b8f1331f688752fa9c
DevanshiBera251 <devanshi.r.bera@kipi.bi>
2024-05-24 14:20:30 +0530

    state

--- models/view/v_order.sql
+++ models/view/v_order.sql
@@ -2,7 +2,7 @@ with monthly_order_summary AS (
     SELECT 
     order_id,
     customer_id,
-    EXTRACT(MONTH FROM order_date) AS order_month,
+    EXTRACT(MONTH FROM order_date) AS month,
     total_amount
 FROM 
     DBT.PUBLIC.orders)
