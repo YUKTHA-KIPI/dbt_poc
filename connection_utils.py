1230c9a215e9ffba449b8625c2b479385cf10456
DevanshiBera251 <140683709+DevanshiBera251@users.noreply.github.com>
2024-06-03 15:28:37 +0530

    Create connection_utils.py

---
+++ connection_utils.py
@@ -0,0 +1,15 @@
+# helpers/connection_utils.py
+import snowflake.snowpark as snowpark
+import os
+def get_session():
+    connection_parameters = {
+        "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+        "user": os.getenv("SNOWFLAKE_USER"),
+        "password": os.getenv("SNOWFLAKE_PASSWORD"),
+        "role": os.getenv("SNOWFLAKE_ROLE"),
+        "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+        "database": os.getenv("SNOWFLAKE_DATABASE"),
+        "schema": os.getenv("SNOWFLAKE_SCHEMA")
+    }
+    session = snowpark.Session.builder.configs(connection_parameters).create()
+    return session
