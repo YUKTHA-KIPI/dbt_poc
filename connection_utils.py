dfb1af232ae2fe1a90e043b0ec8ed276ae514d53
YUKTHA-KIPI <104973913+YUKTHA-KIPI@users.noreply.github.com>
2024-06-10 16:54:48 +0530

    Update connection_utils.py

--- connection_utils.py
+++ connection_utils.py
@@ -3,8 +3,8 @@ import snowflake.snowpark as snowpark
 import os
 def get_session():
     connection_parameters = {
-        "account": "bm12759.ap-south-1.aws",
-        "user": "AMBYINT_TEST",
+        "account": "kx10484.ap-south-1.aws",
+        "user": "AMBYINTTEST",
         "password": "Ambyint1234",
         "role": "ACCOUNTADMIN",
         "warehouse": "COMPUTE_WH",
