141cb432fa450c87a26cba6d4a6da332b0814acf
YUKTHA-KIPI <104973913+YUKTHA-KIPI@users.noreply.github.com>
2024-06-26 12:38:21 +0530

    Create dt_test2.sql

---
+++ models/table/dt_test2.sql
@@ -0,0 +1,10 @@
+{{config(
+    materialized = 'dynamic_table',
+    snowflake_warehouse =compute_wh ,
+    target_lag = '1 hour',
+    on_configuration_change = 'apply'
+)}}
+WITH DIM_EVENT_MAPPING
+AS ( select * from DBT.PUBLIC.EMPLOYEES)
+SELECT * FROM DIM_EVENT_MAPPING
+
